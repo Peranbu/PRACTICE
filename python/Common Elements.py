b0b67933bed1d9227267884ac086a88fc1a7be24
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-01-28 17:26:29 +0530

    Create Common Elements.py

---
+++ python/Common Elements.py
@@ -0,0 +1,7 @@
+def find_common_elements(arr1,arr2):
+  return list(set(arr1)&drt(arr2))
+
+array1=[1,2,3,4,5]
+array2=[3,4,5,6,7]
+common_elements=find_common_elements(array1,array2)
+print(" ",common_elements)
