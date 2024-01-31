4b27b109939c311d04ff9e8f594b6281c1a1bf18
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-01-31 22:39:20 +0530

    Create Perfect number.py

---
+++ python/Perfect number.py
@@ -0,0 +1,14 @@
+def perfect_number(num):
+  sum=0
+  for x in range(1,n):
+    if x%i==0:
+      sum+=x
+    return sum==n
+
+num=int(input("enter the number")
+if perfect_number(num):
+    print("it is a perfect number")
+else:
+    print("it is not a perfect number")
+
+  
