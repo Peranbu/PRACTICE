74215d251017918a54da112776525b9e07ae119e
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-02-02 19:17:01 +0530

    Update factorial.py

--- python/factorial.py
+++ python/factorial.py
@@ -8,6 +8,6 @@ print(factorial(5))
 def factorials(n):
   result=[]
   for i in range(1,n+1):
-    result.append(factorial(n)) #for loop ends here next so be correct on next line indentation to print result correctly
+    result.append(factorial(n)) #for loop ends here and so be correct on next line indentation to print result correctly
   return result
 print (factorials(5))
