6fd0aa0dc26ad4e4713cd4442a8a49c57c945b0a
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-02-06 10:56:43 +0530

    Update Special Characters.py

--- python/Special Characters.py
+++ python/Special Characters.py
@@ -1,12 +1,9 @@
-def special_characters(line):
-  special=[]
-  spaces=0
-  for char in line:
-    if char.isalnum()==False and char !=' ':
-      special.append(char)
-    elif char==' ':
-      spaces+=1
-  print(" Special characters",special)
-  print("Length",len(special))
-  print(" no of spaces",spaces)
-special_characters(":this is a test line !@#$ ")
+def special_character(input):
+  for char in input:
+    if char.isalnum():
+      continue
+    else:
+      print(char,end=" ")
+
+input_string=input(" ")
+special_character(input_string)
