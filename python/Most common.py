c8699ed72d802d510c2cbb9d9e817af8a967491d
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-02-06 19:03:22 +0530

    Update Most common.py

--- python/Most common.py
+++ python/Most common.py
@@ -1,9 +1,20 @@
+#import collection method
 from collections import Counter
-def find_most_common_array(arr):
-  counter=Counter(arr)
-  most_common_elements,count=counter.most_common(1)[0]
-  return most_common_elements
+user_input=input("enter the input")
+list=[int(num) for num in user_input.split(',')]
+c=Counter(list)
+most_common_element=c.most_common(1)[0][0]
+print("the most common",most_common_elements)
 
-array=[1,2,3,4,1,1,1,1,1]
-most_common=find_most_common_array(array)
-print(" ",most_common)
+
+#using if/else
+input
+list=[int(num) for num in user_input.split(',')]
+element_count={}
+for num in element_count:
+  if num in element_count:
+    element_count[num]+=1
+  else:
+    element_count[num]=1
+most_common_elements=max(element_count,key=element_count.get)
+print(" ",most_common_element)
