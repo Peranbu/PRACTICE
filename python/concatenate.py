c364e018529a7836299c90889e02ec7cefab4782
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-02-08 19:02:16 +0530

    Create concatenate.py

---
+++ python/concatenate.py
@@ -0,0 +1,11 @@
+#using +
+string1=int(input("enter the string1"))
+string2=int(input("enter ht estring2"))
+concatenated_string=string1+string2
+print(" ",concatenated_string)
+
+#jusing join
+string1=int(input("enter the string1"))
+string2=int(input("enter ht estring2"))
+concatenated_String=''.join([string1,string2])
+print(" ",concatenated_String)
