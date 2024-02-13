d0226594cf814c009f087d2f19906119ef35953e
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-02-13 11:11:05 +0530

    Create Anagram.py

---
+++ python/Anagram.py
@@ -0,0 +1,11 @@
+def anagram(str1,str2):
+  str1=str1.replace(" ","").lower()
+  str2=str2.replace(" ","").lower()
+  return sorted(str1)==sorted(str2)
+
+word1=input(" ")
+word2=input(" ")
+if anagrams(word1,word2):
+  print("it is anagram")
+else:
+  print("not anagram")
