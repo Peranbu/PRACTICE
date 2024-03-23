2cd9671f4f194e2bdf795c04d4729119e98af166
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-03-23 23:51:34 +0530

    Update and rename Basics.txt to Basics.m

--- SQL/Basics.m
+++ SQL/Basics.m
@@ -4,11 +4,13 @@ from movies
 Movies 
 s.no | titles | age
 1    |tn      | 2
-2    |ap      |3
+2    |ap      | 3
 3    |kl      | 4
-4    |kn      |1
+4    |kn      | 1
 
 rows == records  ||  no.of.rows/records=5(include header)
-column == fields ||  no of columns?fields==3
+column == fields ||  no of columns/fields==3
 
 fields=s.no,titles,age
+
+
