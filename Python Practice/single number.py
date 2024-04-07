c778b3eaf8ac897604c1f3be3e0c3171a91cb6c9
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-04-07 23:59:14 +0530

    Update single number.py

--- Python Practice/single number.py
+++ Python Practice/single number.py
@@ -1,3 +1,5 @@
+#To get list input 1,2,3,4,1,2,3   
+#output=4
 nums = list(map(int, input("Enter numbers separated by space: ").split(",")))
 result = 0
 for num in nums:
@@ -5,6 +7,9 @@ for num in nums:
 print(result) 
 
 
+
+#To get input like [1,2,3,4,1,2,3]
+#output=4
 import ast
 def singleNumber(nums):
     result = 0
@@ -13,6 +18,5 @@ def singleNumber(nums):
     return result
 # Get input from the user
 nums = ast.literal_eval(input("Enter numbers  "))
-
 # Test the function
 print(singleNumber(nums))
