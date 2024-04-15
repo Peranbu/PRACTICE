1b98d6f066fe8d59d2e3952d25beef8760d20190
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-04-15 17:38:31 +0530

    Update No of Rows and Column using python(pandas).py

--- Python Practice/Pandas/No of Rows and Column using python(pandas).py
+++ Python Practice/Pandas/No of Rows and Column using python(pandas).py
@@ -1,5 +1,16 @@
+#Answer 1 
 import pandas as pd
 def getDataframeSize(players: pd.DataFrame) -> List[int]: #the output should be in list of int
     row,col=players.shape  #shape is used to find rows and column
     return[row,col]
-  
+
+
+#Answer 2
+import pandas as pd
+def getDataframeSize(players):
+    """
+    :param players: pandas DataFrame
+    :return: list of two integers representing the number of rows and columns
+    """
+    row,col=players.shape
+    return [row,col]
