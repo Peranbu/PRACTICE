59d1826a6f74c2fbf4052664044672a85ef63124
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-04-16 23:51:50 +0530

    Create Remove duplicate elements using linkedlist.py

---
+++ Python Practice/Remove duplicate elements using linkedlist.py
@@ -0,0 +1,14 @@
+def RemoveDuplicates(head):
+  pointer==head
+  if head is not None:
+    while pointer.next is not None:
+      if pointer.val=pointer.next.val:
+        pointer.next=point.val.val
+      else:
+        pointer=pointer.next
+  return head
+
+
+    
+      
+      
