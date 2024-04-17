c88c525adae4ce15383a4cfc009f763a0f223a7f
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-04-17 21:50:19 +0530

    Create Data Melt.py

---
+++ Python Practice/Pandas/Data Melt.py
@@ -0,0 +1,34 @@
+import pandas as pd
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+  return pd.melt(report,id_vars=['product'],var_name='quarter',value_name='sales')
+
+  """
+  Input:
++-------------+-----------+-----------+-----------+-----------+
+| product     | quarter_1 | quarter_2 | quarter_3 | quarter_4 |
++-------------+-----------+-----------+-----------+-----------+
+| Umbrella    | 417       | 224       | 379       | 611       |
+| SleepingBag | 800       | 936       | 93        | 875       |
++-------------+-----------+-----------+-----------+-----------+
+Output:
++-------------+-----------+-------+
+| product     | quarter   | sales |
++-------------+-----------+-------+
+| Umbrella    | quarter_1 | 417   |
+| SleepingBag | quarter_1 | 800   |
+| Umbrella    | quarter_2 | 224   |
+| SleepingBag | quarter_2 | 936   |
+| Umbrella    | quarter_3 | 379   |
+| SleepingBag | quarter_3 | 93    |
+| Umbrella    | quarter_4 | 611   |
+| SleepingBag | quarter_4 | 875   |
++-------------+-----------+-------+
+
+id_vars=to create a new column
+var_name=to split the questers
+value_name=to split the sales
+
+"""
+
+
+                 
