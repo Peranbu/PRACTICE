fd5f75ea80f8752643892d529a384d1120344a75
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-06-13 11:01:29 +0530

    Create Minimum Number of Moves to Seat Everyone.py

---
+++ Python Practice/Minimum Number of Moves to Seat Everyone.py
@@ -0,0 +1,34 @@
+def NoSeats(seats,students):
+  students.sort()
+  seats.sort()
+  ans=0
+  for i in range(len(seats)):
+    ans+=abs(seats[i]-students[i])  #abs used to give absolute of a value 
+    # a=-15 --> abs(a)=15
+  return ans
+
+seats=int(input(" "))
+students=int(input(" "))
+print("output",NoSeats(seats,students))
+
+"""
+Example 1:
+Input: seats = [3,1,5], students = [2,7,4]
+Output: 4
+Explanation: The students are moved as follows:
+- The first student is moved from from position 2 to position 1 using 1 move.
+- The second student is moved from from position 7 to position 5 using 2 moves.
+- The third student is moved from from position 4 to position 3 using 1 move.
+In total, 1 + 2 + 1 = 4 moves were used.
+
+Example 2:
+Input: seats = [4,1,5,9], students = [1,3,2,6]
+Output: 7
+Explanation: The students are moved as follows:
+- The first student is not moved.
+- The second student is moved from from position 3 to position 4 using 1 move.
+- The third student is moved from from position 2 to position 5 using 3 moves.
+- The fourth student is moved from from position 6 to position 9 using 3 moves.
+In total, 0 + 1 + 3 + 3 = 7 moves were used.
+
+"""
