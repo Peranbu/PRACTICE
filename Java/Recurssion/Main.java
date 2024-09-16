7cd81503c810418efffcf691e537278085d5f67f
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-09-16 19:36:25 +0530

    01.Combination Sum(one) .java

--- Java/Recurssion/Main.java
+++ Java/Recurssion/Main.java
@@ -1,21 +1,32 @@
 import java.util.*;
 public class Main{
-    public static void main(String[] args){
-        int arr[]={10,20,20,10,10,30,50,10,20};
-        HashMap<Integer,Integer> m=new HashMap<>();
-        int pair=0;
-        for(int i=0;i<arr.length;i++){
-            if(m.containsKey(arr[i])){
-                m.put(arr[i],m.get(arr[i])+1);
-                if(m.get(arr[i])>=2){
-                    pair++;
-                    m.put(arr[i],m.get(arr[i])-2);
-                    System.out.println(m);
-                }
-            }else{
-                m.put(arr[i],1);
+    public void findCombinations(int ind,int[] arr,int target,List<List<Integer>>ans,List<Integer>ds){
+        if(ind==arr.length){
+            if(target==0){
+                ans.add(new ArrayList<>(ds)); //base case
             }
+            return;
         }
-        System.out.println(m);
+        if(arr[ind]<=target){
+            ds.add(arr[ind]);
+            findCombinations(ind,arr,target-arr[ind],ans,ds); //add the number(leftmost--1st) 
+            ds.remove(ds.size()-1); //remove the above added number
+        }
+        findCombinations(ind+1,arr,target,ans,ds); //repeat the process(left to right)
+    }
+    public List<List<Integer>> combinationSum(int[] candidate,int target){
+        List<List<Integer>> ans=new ArrayList<>();
+        findCombinations(0,candidate,target,ans,new ArrayList<>()); // store the ans
+        //stack data structure is used to push all the numbers to get to the target and finally print the output
+        return ans;
+    }
+    public static void main(String[] args){
+        Main obj=new Main();
+        int[] candidates={1,2,3,4,5};
+        int target=4;
+        List<List<Integer>> result=obj.combinationSum(candidates,target);
+        for(List<Integer> okie:result){
+            System.out.println(okie);
+        }
     }
 }
