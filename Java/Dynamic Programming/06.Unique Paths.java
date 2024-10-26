9eb7d3014ca3a82e600bdb48ec744171a225f35d
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-10-26 17:54:53 +0530

    Update 06.Unique Paths.java

--- Java/Dynamic Programming/06.Unique Paths.java
+++ Java/Dynamic Programming/06.Unique Paths.java
@@ -34,3 +34,42 @@ class Main{
 }
 
 //tabulation approach
+//iterate using for loop
+//base case
+//up and left
+//return answer upto last element
+import java.util.*;
+class Main{
+    static int countWaysUntil(int m,int n,int[][] dp){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0&&j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                int up=0;
+                int left=0;
+                if(i>0){
+                    up=dp[i-1][j];
+                }
+                if(j>0){
+                    left=dp[i][j-1];
+                }
+                dp[i][j]=up+left;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    static int countWays(int m,int n){
+        int dp[][]=new int[m][n];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+        return countWaysUntil(m,n,dp);
+    }
+    public static void main(String[] args){
+        int m=3;
+        int n=2;
+        System.out.println(countWays(m,n));
+    }
+}
