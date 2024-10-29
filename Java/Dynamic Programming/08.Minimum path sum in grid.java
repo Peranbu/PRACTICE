594380d457800a8b1b1d48ac7cfb7e11cf830856
Peranbu <112739680+Peranbu@users.noreply.github.com>
2024-10-29 19:35:43 +0530

    Update 08.Minimum path sum in grid.java

--- Java/Dynamic Programming/08.Minimum path sum in grid.java
+++ Java/Dynamic Programming/08.Minimum path sum in grid.java
@@ -1,3 +1,4 @@
+//memorization
 import java.util.*;
 class Main{
     static int solveAnswer(int i,int j,int[][] matrix,int[][] dp){//base cases
@@ -27,3 +28,41 @@ class Main{
         System.out.println(solve(m,n,matrix));
     }
 }
+//at maximum condition -- O(M*N)
+//space -- O(M-1)+O(N-1)+O(M*N)
+
+
+//tabulation
+import java.util.*;
+class Main {
+    static int minimumPath(int m, int n, int[][] matrix) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = matrix[i][j];
+                } else {
+                    int up = Integer.MAX_VALUE;
+                    int left = Integer.MAX_VALUE;
+                    if (i > 0) {
+                        up = matrix[i][j] + dp[i - 1][j];
+                    }
+                    if (j > 0) {
+                        left = matrix[i][j] + dp[i][j - 1];
+                    }
+                    dp[i][j] = Math.min(up, left);
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+    public static void main(String[] args) {
+        int[][] matrix = { {5, 9, 6}, {11, 5, 2} };
+        int m = matrix.length;
+        int n = matrix[0].length;
+        System.out.println(minimumPath(m, n, matrix));
+    }
+}
+
+// time complexity-O(M*N) -- 2 nested loops
+//space-- O(M*N)
